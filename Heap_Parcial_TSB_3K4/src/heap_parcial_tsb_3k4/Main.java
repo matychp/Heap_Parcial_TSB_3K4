e8670267ee98004bcf5d60122c5bb8d5d3477302
matychp <matychp@gmail.com>
2014-10-01 18:25:52 -0300

    Metodo lucky() corregido. Implementado metodo getBoth(). Implementado metodo removeBoth(). Implementado opciones de main para las opciones anteriores.

--- Heap_Parcial_TSB_3K4/src/heap_parcial_tsb_3k4/Main.java
+++ Heap_Parcial_TSB_3K4/src/heap_parcial_tsb_3k4/Main.java
@@ -15,24 +15,35 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {        
-        Heap <Integer> ah = new Heap<>(6, true);
-        ah.add(8);
+        Heap <Integer> ah = new Heap<>(6, false);
+        ah.add(5);
         ah.add(3);
         ah.add(7);
         ah.add(9);
         ah.add(4);
         ah.add(2);
-        System.out.println("Heap ascendente: " + ah);
-        System.out.println(ah.lucky(1));
-        System.out.println(ah.lucky(10));
-        
-        System.out.print("Mostramos en orden de extraccion: \n[ ");
-        while( ! ah.isEmpty() )
-        {
-            int x = ah.remove();
-            System.out.print(x + " ");
+//        System.out.println("Lucky?: " + ah.lucky(1));
+//        System.out.println("Lucky?: " + ah.lucky(10));
+        SimpleList dosOptimosGet = ah.getBoth();
+        if(dosOptimosGet != null){
+            System.out.println(dosOptimosGet.toString());
+        }else{
+            System.out.println("No existen dos optimos (hay 0 o 1 elemento).");
         }
-        System.out.println("]\n");
+        
+        SimpleList dosOptimosRemove = ah.removeBoth();
+        if(dosOptimosRemove != null){
+            System.out.println(dosOptimosRemove.toString());
+        }else{
+            System.out.println("No existen dos optimos (hay 0 o 1 elemento).");
+        }
+//        System.out.print("Mostramos en orden de extraccion: \n[ ");
+//        while( ! ah.isEmpty() )
+//        {
+//            int x = ah.remove();
+//            System.out.print(x + " ");
+//        }
+//        System.out.println("]\n");
     }
     
 }
