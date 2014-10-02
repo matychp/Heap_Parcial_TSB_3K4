27cd188fcfab363d2d58b63fe8cc0d3bd12c9788
matychp <matychp@gmail.com>
2014-10-01 21:06:05 -0300

    Añadido ccomentarios a la clase Main, metodo main().

--- Heap_Parcial_TSB_3K4/src/interfaz/Main.java
+++ Heap_Parcial_TSB_3K4/src/interfaz/Main.java
@@ -18,24 +18,38 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Creo un Heap de tamaño 6, inicialmente.
         Heap<Integer> ah = new Heap<>(6, true);
+        
+        //Se agregan algunos elementos. Y el Heap se ordena cada vez que se agrega uno.
         ah.add(5);
         ah.add(12);
         ah.add(20);
         ah.add(8);
         ah.add(10);
         ah.add(11);
-        System.out.println("Lucky?: " + ah.lucky(1));
-        System.out.println("Lucky?: " + ah.lucky(10));
+        
+        //Se ponen dos ejemplos del metodo lucky(). Si el Heap es ascendente, el primer lucky() da true, y el siguiente false.
+        //En caso de que el Heap sea descendente, el primero devuelve false, y el segundo true.
+        System.out.println("Lucky?: " + ah.lucky(4));
+        System.out.println("Lucky?: " + ah.lucky(30));
+        
+        //Se obtienen los dos optimos.
+        //Si el Heap es ascendente, debe devolver la raiz y el hijo menor.
+        //Si el Heap es descendente, debe devolver la raiz y el hijo mayor.
         SimpleList dosOptimosGet = ah.getBoth();
-        System.out.println(dosOptimosGet.toString());
+        System.out.println("Optimos: " + dosOptimosGet.toString());
 
+        //Lo mismo que el getBoth(), pero esta vez elimina los optimos.
         SimpleList dosOptimosRemove = ah.removeBoth();
-        System.out.println(dosOptimosRemove.toString());
-        System.out.println(ah.toString());
+        System.out.println("Removiendo los dos optimos: " + dosOptimosRemove.toString());
+        
+        //Vector ordenado en forma de arbol binario completo o casi completo.
+        System.out.println("Vector tipo arbol binario: " + ah.toString());
 
-        System.out.print(
-                "Mostramos en orden de extraccion: \n[ ");
+        //A continuacion se extrae del Heap uno a uno el menor(si es ascendente) o el mayor(descendente).
+        //Y se muestran por pantalla.
+        System.out.print("Mostramos en orden de extraccion: \n[ ");
         while (!ah.isEmpty()) {
             int x = ah.remove();
             System.out.print(x + " ");
